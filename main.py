40d1d170d020b9256c624e975d23566b7b50a62e
Dan168 <93716943+Dan168@users.noreply.github.com>
2022-11-13 14:16:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ data = pd.read_csv('AAPL.csv')
 X = data.iloc[:, 0].values.reshape(-1,1)
 Y = data.iloc[:, 1].values.reshape(-1,1)
 
-print("Analysing past 127 trading days of APPL. Please wait.")
+print("Analysing past 127 trading days of AAPL. Please wait.")
 
 lr = LinearRegression()
 lr.fit(X,Y)
@@ -18,8 +18,8 @@ plt.scatter(X, Y)
 plt.plot(X, Y_pred, color='red')
 plt.show()
 
-userInput = input("How many days from non would you like to predict?")
+userInput = input("How many days from now would you like to predict?")
 pred_days = (int(userInput) + 127)
 
-print("APPL Price prediction " + str(userInput) + " days from now:")
+print("AAPL Price prediction " + str(userInput) + " days from now:")
 print(lr.predict(np.array([pred_days]).reshape(-1, 1)))
